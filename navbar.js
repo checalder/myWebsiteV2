1b393ebf0bca954ab64112141e35ad997f40e0d8
che laptop <ccald004@campus.goldsmiths.ac.uk>
2024-06-14 00:23:39 +0100

    added from v1

---
+++ navbar.js
@@ -0,0 +1,8 @@
+var bar = document.createElement("div");
+bar.id = "bar";
+document.body.appendChild(bar);
+
+var button = document.createElement("button");
+button.href
+button.innerHTML = "CLICK";
+bar.appendChild(button);
